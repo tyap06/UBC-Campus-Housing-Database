59d78cf15792aafded72e7e4ecabae70a8156c8d
UBC Student <vihangip@students.cs.ubc.ca>
2022-11-22 01:46:09 -0800

    Add files via upload

---
+++ Student Housing/src/Database/RoomAllocationInfoHandler.java
@@ -0,0 +1,55 @@
+
+package database;
+
+import model.model;
+import model.RoomAllocationInfo;
+
+
+import java.sql.*;
+import util.Constants;
+import java.util.ArrayList;
+
+private class RoomAllocationInfoHandler implements ModelHandler {
+
+    @Override
+    public void insert(Model model, Connection connection) {
+        System.out.println(Constants.WARNING_TAG + "You cannot insert to this Table");
+    }
+
+    @Override
+    public void update(Model model, Connection connection) {
+        System.out.println(Constants.WARNING_TAG + "You cannot update this Table");
+    }
+
+    @Override
+    public void delete(Model model, Connection connection) {
+        System.out.println(Constants.WARNING_TAG + "You cannot delete from this Table");
+    }
+
+    @Override
+    public Model[] getInfo(Connection connection) {
+        ArrayList<RoomAllocationInfo> result = new ArrayList<>();
+
+        try {
+            String query = "SELECT * FROM RoomAllocationInfo";
+            PreparedStatement ps = connection.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()) {
+                RoomAllocationInfo roomAllocationInfo = new RoomAllocationInfo(rs.getString("House"),
+                        rs.getInt("unitNumber"),
+                        rs.getInt("residentID"),
+                        rs.getInt("residenceID"));
+                result.add(roomAllocationInfo);
+            }
+
+            rs.close();
+            ps.close();
+        } catch (SQLException e) {
+            System.out.println(Constants.EXCEPTION_TAG + " " + e.getMessage());
+        }
+
+        return result.toArray(new RoomAllocationInfo[0]);
+    }
+
+}
