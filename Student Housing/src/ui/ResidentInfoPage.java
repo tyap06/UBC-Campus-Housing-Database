b4f1456174a7608996f794acf8f4c448bd1626f1
Tim <tyap1106@students.cs.ubc.ca>
2022-11-25 17:59:43 -0800

    Implemented update query button on ResidentInfoPage

--- Student Housing/src/ui/ResidentInfoPage.java
+++ Student Housing/src/ui/ResidentInfoPage.java
@@ -1,10 +1,15 @@
 package ui;
 
+import controller.Housing;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
-public class ResidentInfoPage extends JFrame {
+public class ResidentInfoPage extends JFrame implements ActionListener {
+    Housing housing = new Housing();
     JLabel nameLabel = new JLabel();
     JLabel housingSessionLabel = new JLabel();
     JLabel houseLabel = new JLabel();
@@ -14,6 +19,8 @@ public class ResidentInfoPage extends JFrame {
     JLabel rentLabel = new JLabel();
     JLabel visitorUpdateLabel = new JLabel();
     JLabel visitorUpdateLabel2 = new JLabel();
+    JLabel visitorEmailLabel = new JLabel();
+    JTextField visitorEmailTextField = new JTextField(30);
     JTextField visitorUpdateTextField = new JTextField(30);
     JButton visitorUpdateButton = new JButton("Update");
     JPanel panel = new JPanel();
@@ -34,6 +41,7 @@ public class ResidentInfoPage extends JFrame {
         roomTypeLabel.setText("Room Type: " + ls.get(5));
         rentLabel.setText("Rent: " + ls.get(6));
         visitorUpdateLabel.setText("For Visitors Only: If you would like to update your length of stay,");
+        visitorEmailLabel.setText("Enter email address below:");
         visitorUpdateLabel2.setText("Enter the updated length of stay below:");
 
         panel.add(nameLabel);
@@ -44,11 +52,27 @@ public class ResidentInfoPage extends JFrame {
         panel.add(roomTypeLabel);
         panel.add(rentLabel);
         panel.add(visitorUpdateLabel);
+        panel.add(visitorEmailLabel);
+        panel.add(visitorEmailTextField);
         panel.add(visitorUpdateLabel2);
         panel.add(visitorUpdateTextField);
         panel.add(visitorUpdateButton);
 
+        //perform action on button click
+        visitorUpdateButton.addActionListener(this);
+
         add(panel);
 
     }
+    public void actionPerformed(ActionEvent ae){
+        String updateEmailValue = visitorEmailTextField.getText(); // gets email address from visitor
+        String updateUserValue = visitorUpdateTextField.getText(); // gets updated length of stay from user input
+
+        if(ae.getSource() == visitorUpdateButton) {
+            housing.updateQuery(updateEmailValue, updateUserValue);
+            JOptionPane confirmationPane = new JOptionPane();
+            confirmationPane.setMessage("Length of stay updated");
+        }
+    }
+
 }
